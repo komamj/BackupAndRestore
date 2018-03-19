786125cfdbe73495e3d90286c3b440c8b3db4fb7
koma_mj <junmaokoma@gmail.com>
2018-03-19 20:29:48 +0800

    BackupAndRestore: Init project.

---
+++ BackupAndRestore1/app/src/main/java/com/koma/backuprestore/MainActivity.java
@@ -0,0 +1,16 @@
+package com.koma.backuprestore;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by koma_20 on 2018/3/19.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
